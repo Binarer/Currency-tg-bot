9282320c48e8ad1eb04eb0500c5ed356f83a947d
lasndnansdl <flihjkcey@gmail.com>
2024-12-03 13:46:21 +0500

    add docs

--- src/main/java/easy/currencytgbot/Bot/infrastructure/States/FromCurrencyState.java
+++ src/main/java/easy/currencytgbot/Bot/infrastructure/States/FromCurrencyState.java
@@ -5,7 +5,7 @@ import easy.currencytgbot.Bot.Application.Interfaces.UserState;
 import easy.currencytgbot.Bot.infrastructure.Components.BotCommands;
 import easy.currencytgbot.Bot.infrastructure.Components.Buttons;
 
-public class FromCurrencyState implements UserState {
+public class  FromCurrencyState implements UserState {
     @Override
     public void handle(String input, long chatId, Bot bot) {
         bot.getUserStateStorage().setUserFromCurrency(chatId, input);
